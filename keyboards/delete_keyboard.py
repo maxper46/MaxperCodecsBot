7fa5a0fb9ec29cad96ec90f66a5655f8f6b3f7af
Maxim Perekhrest <maxper@list.ru>
2024-08-26 16:47:23 +0700

    Errors are ignored when editing the menu of old messages.

--- keyboards/delete_keyboard.py
+++ keyboards/delete_keyboard.py
@@ -1,9 +1,16 @@
 from aiogram import Bot
+from aiogram.exceptions import TelegramBadRequest
 
 
 async def del_keyboard(message_id: int, chat_id: int, key: int, bot: Bot) -> None:
     if message_id:
         if key:
-            await bot.delete_message(message_id=message_id, chat_id=chat_id)
+            try:
+                await bot.delete_message(message_id=message_id, chat_id=chat_id)
+            except TelegramBadRequest:
+                return
         else:
-            await bot.edit_message_reply_markup(message_id=message_id, chat_id=chat_id)
+            try:
+                await bot.edit_message_reply_markup(message_id=message_id, chat_id=chat_id)
+            except TelegramBadRequest:
+                return
